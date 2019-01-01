4a37e5f370de9da67c5b485e175062c8b1871692
Jonathan <jonathanwmeier@gmail.com>
2019-01-01 19:12:48 +0000

    Add client code.

---
+++ src/docli/client.py
@@ -0,0 +1,38 @@
+import digitalocean
+
+from docli import config
+
+
+class DOClient:
+
+    def __init__(self, token):
+
+        self.token = token
+
+    def _block_on_create_droplet(self, droplet: digitalocean.Droplet) -> None:
+        """
+        Block on droplet creation.
+        """
+
+        actions = droplet.get_actions()
+
+        for action in actions:
+            action.load()
+            # Once it shows complete, droplet is up and running
+            print(f'Droplet Status: {action.status}')
+
+    def create_droplet(self, *args, **kwargs) -> digitalocean.Droplet:
+        """
+        Create a DO droplet based on called specs.
+
+        Optionally block on creation of the droplet.
+        """
+
+        droplet = digitalocean.Droplet(
+                    token=self.token,
+                    **kwargs,
+                )
+
+        self._block_on_create_droplet(droplet)
+
+        return droplet
