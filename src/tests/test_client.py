01d5d88f88b0fb168b16b3209e605f33e7b98eb9
Jonathan <jonathanwmeier@gmail.com>
2019-01-01 19:11:49 +0000

    Add test for client code.

---
+++ src/tests/test_client.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest import mock
+
+from docli.client import DOClient
+
+class DOClientTestCase(TestCase):
+
+    @mock.patch('docli.client.DOClient._block_on_create_droplet')
+    @mock.patch('docli.client.digitalocean.Droplet.__init__')
+    def test_create_droplet(self, *mocks):
+        """
+        Test that we can successfully create a droplet with passthrough args.
+        """
+        mocks[0].return_value = None
+
+        token = 'test'
+        client = DOClient(token)
+
+        kwargs = {
+                'name':'Example',
+                'region':'nyc2', # New York 2
+                'image':'ubuntu-14-04-x64', # Ubuntu 14.04 x64
+                'size_slug':'512mb',  # 512MB
+                'backups':False,
+                }
+
+
+        client.create_droplet(**kwargs)
+
+        mocks[0].assert_called_with(token=client.token,**kwargs)
+
